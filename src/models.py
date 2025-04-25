266b65ae54ced6391f2813fb55c6f9410ad363d8
Diego Vázquez Pérez <diego_28c@hotmail.com>
2025-04-25 00:49:07 +0000

    finalizado

--- src/models.py
+++ src/models.py
@@ -1,19 +1,66 @@
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
 
-db = SQLAlchemy()
+Base = declarative_base()
 
-class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(String(15), primary_key=True) 
+    email = Column(String(25), nullable=False) 
+    password = Column(String(30), nullable=False) 
+    posts = relationship("Post", back_populates="autor") 
+    likes = relationship("Likes", back_populates="user")  
+    def to_dict(self):
+        return {}
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(String(15), primary_key=True) 
+    user_id = Column(String(15), ForeignKey('user.id')) 
+    content = Column(String(250), nullable=True) 
+    autor = relationship("User ", back_populates="posts") 
+    likes = relationship("Likes", back_populates="post") 
+
+    def to_dict(self):
+        return {}
+
+class Likes(Base): 
+    __tablename__ = 'likes' 
+    id = Column(Integer, primary_key=True) 
+    user_id = Column(String(15), ForeignKey('user.id'))  
+    post_id = Column(String(15), ForeignKey('post.id'))  
+    user = relationship("User ", back_populates="likes") 
+    post = relationship("Post", back_populates="likes") 
+
+    def to_dict(self):
+        return {}
+
+class Follower(Base): 
+    __tablename__ = 'follower'  
+    id = Column(Integer, primary_key=True) 
+    follower_id = Column(String(15), ForeignKey('user.id'))  
+    user_id = Column(String(15), ForeignKey('user.id'))  
+
+    def to_dict(self):
+        return {}
+
+class Following(Base): 
+    __tablename__ = 'following' 
+    id = Column(Integer, primary_key=True) 
+    follower_id = Column(String(15), ForeignKey('user.id')) 
+    user_id = Column(String(15), ForeignKey('user.id'))  
+
+    def to_dict(self):
+        return {}
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem generating the diagram")
+    raise e
